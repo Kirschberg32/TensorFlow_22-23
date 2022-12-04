7aa00a1cd8408bc8445467ba98fc7559b4959836
egrund <82370915+egrund@users.noreply.github.com>
2022-12-04 12:47:38 +0100

    enabled visualization of data sample for homework 5

--- Homework05/main.py
+++ Homework05/main.py
@@ -10,7 +10,7 @@ optimizer = tf.optimizers.Adam(learning_rate = LEARNING_RATE)
 EPOCHS = 15
 
 # get and prepare data and model
-(training_data, test_data ) , _ = get_data.load_data(False)
+(training_data, test_data ) , _ = get_data.load_data(True)
 training_data = training_data.apply(get_data.data_preprocess)
 test_data = test_data.apply(get_data.data_preprocess)
 
