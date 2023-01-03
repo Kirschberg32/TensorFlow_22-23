def9584e17e8a73517a407e0400712cf6a674b7e
Kirschberg32 <82382243+Kirschberg32@users.noreply.github.com>
2023-01-03 16:32:23 +0100

    Run-2 added
    Add run 2 and additionally save plots automaticly

--- Homework07/main.py
+++ Homework07/main.py
@@ -3,6 +3,9 @@ from model import MyLSTMModel
 import tensorflow_datasets as tfds
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import os
+
+config = "run2"
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
 loss = tf.keras.losses.MeanSquaredError()
@@ -22,15 +25,17 @@ model = MyLSTMModel(total_input_shape = (batch_size, sequence, 28,28,1), lstm_un
 # compile the model (here, adding a loss function and an optimizer)
 model.compile(optimizer = optimizer, loss=loss)
 
-logging_callback = tf.keras.callbacks.TensorBoard(log_dir="./logs/run1")
+logging_callback = tf.keras.callbacks.TensorBoard(log_dir=f"./logs/{config}")
 
 history = model.fit(training_data, validation_data = val_data, epochs=epochs, batch_size=batch_size, callbacks=[logging_callback])
+os.makedirs(f"Plots/", exist_ok = True)
 
-model.save("saved_model/run1")
+model.save(f"saved_model/{config}")
 
 plt.plot(history.history["loss"])
 plt.plot(history.history["val_loss"])
 plt.legend(labels=["training","validation"])
 plt.xlabel("Epoch")
 plt.ylabel("Mean Squared Error")
+plt.savefig(f"Plots/{config}.png")
 plt.show()
