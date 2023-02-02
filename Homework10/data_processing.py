8232506104260af7a375cc85fb5779da119bd3c9
Kirschberg32 <82382243+Kirschberg32@users.noreply.github.com>
2023-02-02 04:51:31 +0100

    Adding Timer

--- Homework10/data_processing.py
+++ Homework10/data_processing.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import time
 
 def load_data(path):
     with open(path) as f:
@@ -23,33 +24,37 @@ def get_preprocessed_data(path : str, most_common_size : int = 10000,window_size
     """ loads and fully prepares the dataset, returns the tokanizer for later usage"""
     
     data = load_data(path)
+    
     #tokenization
+    print("Initilaize creating Tokens:")
+    sec = time.time()
     tokenizer = tf.keras.preprocessing.text.Tokenizer(
         num_words=most_common_size,
         filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n1234567890',
     )
-    print("After getting tokenizer")
+    
     tokenizer.fit_on_texts(data)
-    print("After fit on text")
     tokens = tokenizer.texts_to_sequences(data)
-    print("after text to sequence")
     tokens = [i for sublst in tokens for i in sublst if i]
-    print("flattend")
-
+    print("Done: ", round(time.time() - sec, 4) ,"sec")
+    
+    print("Initialize Paring:")
+    sec = time.time()
     # pairing
     pairs, targets = tf.keras.preprocessing.sequence.skipgrams(tokens, most_common_size, window_size)# default shuffle here and creates negative samples
-
-    print("done pairing")
-
+    print("Done: ", round(time.time() - sec, 4) ,"sec")
+    
+    
     # split in train and test set
     pairs_len = len(pairs)
     print("Pairs length: ", pairs_len)
     train_len = int(pairs_len * train_part)
     train_pairs, test_pairs = pairs[:train_len], pairs[train_len:]
     train_targets, test_targets = targets[:train_len], targets[train_len:]
-    print("done splitting")
-
-    # tf Dataset and Preprocess
+    
+    print("Create Datasets and initialize Preprocess:")
+    sec = time.time()
+    # tf Dataset and Preprocess    
     train_ds = tf.data.Dataset.from_tensor_slices(train_pairs)
     test_ds = tf.data.Dataset.from_tensor_slices(test_pairs)
     train_targets = tf.data.Dataset.from_tensor_slices(train_targets)
@@ -58,10 +63,8 @@ def get_preprocessed_data(path : str, most_common_size : int = 10000,window_size
     train_ds = tf.data.Dataset.zip((train_ds,train_targets))
     test_ds = tf.data.Dataset.zip((test_ds,test_targets))
 
-
-    print("done as Datasets")
     train_ds = data_preprocess(train_ds)
     test_ds = data_preprocess(test_ds)
-    print("Done preprocessing")
-
+    print("Done: ", round(time.time() - sec, 4) ,"sec")
+    
     return (train_ds, test_ds), tokenizer
