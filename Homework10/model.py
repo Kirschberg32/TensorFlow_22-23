41a77a07a46d11eb81f9906cfebb84a731f0c5e8
egrund <82370915+egrund@users.noreply.github.com>
2023-02-06 11:24:37 +0100

    debugged model for now

--- Homework10/model.py
+++ Homework10/model.py
@@ -11,8 +11,8 @@ class SkipGram(tf.keras.models.Model):
 
         self.metrices = [tf.keras.metrics.Mean(name = "loss")]
 
-    #def build(self, input_shape):
-        #self.num_sampled = input_shape[2]
+    def build(self, input_shape):
+        self.num_sampled = 1
         self.embedding = self.add_weight(
             name = "embedding",
             shape=(self.vocabulary_size,self.embedding_size),
@@ -31,35 +31,45 @@ class SkipGram(tf.keras.models.Model):
             initializer='zero',
             trainable=True
         )
+        #super().build(input_shape)
 
     def reset_metrics(self):
         """ resets all the metrices that are observed during training and testing """
         for m in self.metrics:
             m.reset_states()
 
-    @tf.function
+    #@tf.function
     def call(self, input, training = None): # not done
-        target_embedding = tf.nn.embedding_lookup(self.embedding, input[:][0])
+        target_embedding = tf.squeeze(tf.nn.embedding_lookup(self.embedding, tf.cast(input[:,0],tf.int64)))
+
+        #print("weights=",self.score_weight.shape,"\nbiases=",self.score_bias.shape, "\nlabels=",input[:,1].shape,"\ninputs=",target_embedding.shape, "\nnum_true =", self.num_sampled, "\nnum_classes =", self.vocabulary_size)
+
         loss = tf.nn.nce_loss(
                 weights=self.score_weight,#(100,embedding_size)
                 biases=self.score_bias, #(100,)
-                labels=input[:][1], #(batch-size,1)
+                labels=input[:,1], #(batch-size,1)
                 inputs=target_embedding, # (batch_size,embedding_size)
-                num_true = 1, #self.num_sampled, # jetzt 1
+                num_true = self.num_sampled, # jetzt 1
                 num_sampled = self.num_neg_sample, # 1
                 num_classes = self.vocabulary_size # 100
             )
         return loss
 
-    @tf.function
+    #@tf.function
     def train_step(self,inputs):
 
         with tf.GradientTape() as tape:
             loss = self(inputs,True)
-            self.metrics[0].update_state(values = tf.mean(loss,reduce_instance_dims = False)) # loss
+            self.metrics[0].update_state(values = tf.math.reduce_mean(loss,axis=0)) # loss
 
         gradients = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients,self.trainable_variables))
         
         return {m.name : m.result() for m in self.metrics}
 
+def get_Model():
+    inputs = tf.keras.layers.Input(shape=(2,1,),dtype='int32')
+    outputs = SkipGram(100,32,1)(inputs)
+
+    model = tf.keras.models.Model(inputs=[inputs],outputs = [outputs])
+    return model, outputs
