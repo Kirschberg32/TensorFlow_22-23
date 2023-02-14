3fbc207a72b96d2613f1183e5aa06008ff06a77f
egrund <82370915+egrund@users.noreply.github.com>
2023-02-14 09:06:08 +0100

    start model
    not debugged, not done

--- Homework11/main.py
+++ Homework11/main.py
@@ -1,21 +1,31 @@
 import data_processing as dp
+import tensorflow as tf
+from model import MyModel
 
 # variables from prepare, do not change, or otherwise prepare newly
 original_file_path = "data/bible.txt"
 prepared_file_path = "data/prepared_bible.txt"
 model_prefix = 'tokenizer_model'
-VOCABUALRY_SIZE = 2000
-WINDOW_SIZE = 64
+VOCABULARY_SIZE = 2000 # 2000 - 7000
+WINDOW_SIZE = 64 # 32 - 256
 BATCH_SIZE = 64
+EMBEDDING_DIM = 100 # 64 - 256
+NUM_HEADS = 2 # 2-4
+FIRST_UNIT = 32 # 32-256
+
+# variables for the model
+optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+loss_function = tf.keras.losses.CategoricalCrossentropy(from_logits = True)
 
 # prepare if you want to create a new tokenizer and a new prepared data file
-data, prepared_data, tokenizer = dp.prepare_everything(original_file_path,prepared_file_path,model_prefix,VOCABUALRY_SIZE)
+data, prepared_data, tokenizer = dp.prepare_everything(original_file_path,prepared_file_path,model_prefix,VOCABULARY_SIZE)
 
 # if you only want to create a new tokenizer first, use loading afterwards
-# dp.train_tokenizer(prepared_file_path,VOCABUALRY_SIZE,model_prefix)
+# dp.train_tokenizer(prepared_file_path,VOCABULARY_SIZE,model_prefix)
 
 # load everything if already prepared
 # data, prepared_data, tokenizer = dp.prepare_everything(original_file_path,prepared_file_path,model_prefix)
 
 dataset = dp.create_dataset(prepared_data,tokenizer,WINDOW_SIZE, BATCH_SIZE)
 
+# model = MyModel(tokenizer, optimizer, loss_function, VOCABULARY_SIZE, EMBEDDING_DIM, NUM_HEADS, FIRST_UNIT)
---
+++ Homework11/model.py
@@ -0,0 +1,105 @@
+import tensorflow as tf
+
+class MyEmbedder(tf.keras.layers.Layer):
+
+    def __init__(self,vocabulary_size,embedding_dim,sequence_length):
+        super().init()
+        self.sequence_length = sequence_length
+        self.embedding_layer = tf.keras.layers.Embedding(input_dim = vocabulary_size, output_dim = embedding_dim)
+        self.second_embedding_layer = tf.keras.layers.Embedding(input_dim = sequence_length, output_dim = embedding_dim)
+        self.add_layer = tf.keras.layers.Add()
+
+    def __call__(self, input, training = None):
+        my_indices = tf.range(0,self.sequence_length)
+
+        token_embedding = self.embedding_layer(input)
+        indices_embedding = self.second_embedding_layer(my_indices)
+
+        return self.add_layer(token_embedding,indices_embedding)
+
+class TransformerBlock(tf.keras.layers.Layer):
+
+    def __init__(self, embedding_dim, num_a_heads :int = 2, first_units : int = 32):
+        super().__init__()
+
+        self.mha_layer = tf.keras.layers.MultiHeadAttention(num_heads = num_a_heads, key_dim = embedding_dim)
+
+        self.dense1 = tf.keras.layer.Dense(units = first_units, activation = tf.nn.relu) # between 32 and 256 units
+        self.dense2 = tf.keras.layer.Dense(units = embedding_dim)
+
+        self.dropout1 = tf.keras.layers.Dropout(rate = 0.1)
+        self.dropout2 = tf.keras.layers.Dropout(rate = 0.1)
+
+        self.normalization1 = tf.keras.layers.LayerNormalization(epsilon = 0.000001)
+        self.normalization2 = tf.keras.layers.LayerNormalization(epsilon = 0.000001)
+
+        self.add_layer = tf.keras.layers.Add()
+
+    def __call__(self, input, training = None):
+        
+        x = self.mha_layer(input, use_causal_mask = True)
+        x = self.dropout1(input, training)
+        x = self.add_layer(input, x)
+
+        ln_out = self.normalization1(x,training)
+        x = self.dense1(ln_out)
+        x = self.dense2(x)
+        x = self.dropout2(x, training)
+        x = self.add_layer(ln_out, x)
+
+        x = self.normalization2(x, training)
+        return x
+
+class MyModel(tf.keras.Model):
+
+    def __init__(self,tokenizer, optimizer, loss_function, vocabulary_size : int, embedding_dim : int, num_heads : int = 2, first_units : int = 32):
+        super().__init__()
+
+        self.tokenizer = tokenizer
+        self.optimizer = optimizer
+        self.loss = loss_function
+        self.metrics = [tf.keras.metrics.Mean(name="loss"), tf.keras.metrics.CategoricalAccuracy(name="accuracy")]
+
+        self.embedding_layer = MyEmbedder(vocabulary_size,embedding_dim)
+        self.transformer_block = TransformerBlock(embedding_dim,num_heads, first_units)
+        self.dense = tf.keras.layers.Dense(units = vocabulary_size)
+
+    def reset_metrics(self):
+        for metric in self.metrics:
+            metric.reset_states()
+
+    def __call__(self, input, training = None):
+        x = self.embedding_layer(input, training)
+        x = self.transformer_block(x, training)
+        x = self.dense(x)
+        return x
+
+    @tf.function
+    def train_step(self, data):
+        
+        #x, targets = data
+        
+        with tf.GradientTape() as tape:
+            predictions = self(data, training=True)
+            
+            loss = self.loss_function(data, predictions) + tf.reduce_sum(self.losses)
+        
+        gradients = tape.gradient(loss, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+        
+        # update loss metric
+        self.metrics[0].update_state(loss)
+        
+        # for all metrics except loss, update states (accuracy etc.)
+        for metric in self.metrics[1:]:
+            metric.update_state(data,predictions)
+
+        # Return a dictionary mapping metric names to current value
+        return {m.name: m.result() for m in self.metrics}
+
+    def generate_text(self, prompt, output_length, top_k):
+        pass
+
+
+
+
